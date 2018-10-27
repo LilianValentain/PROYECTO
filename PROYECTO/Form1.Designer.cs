febd653be532418a8ee1b61bb5406cd4b195c764
jp50000 <44383527+jp50000@users.noreply.github.com>
2018-10-26 23:42:13 -0700

    modificado

--- PROYECTO/Form1.Designer.cs
+++ PROYECTO/Form1.Designer.cs
@@ -28,20 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.paneljuego = new System.Windows.Forms.FlowLayoutPanel();
+            this.cmdReinicio = new System.Windows.Forms.Button();
+            this.lblRecord = new System.Windows.Forms.Label();
+            this.lblDijito = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // paneljuego
+            // 
+            this.paneljuego.Location = new System.Drawing.Point(12, 12);
+            this.paneljuego.Name = "paneljuego";
+            this.paneljuego.Size = new System.Drawing.Size(422, 308);
+            this.paneljuego.TabIndex = 1;
+            // 
+            // cmdReinicio
+            // 
+            this.cmdReinicio.Location = new System.Drawing.Point(463, 12);
+            this.cmdReinicio.Name = "cmdReinicio";
+            this.cmdReinicio.Size = new System.Drawing.Size(111, 23);
+            this.cmdReinicio.TabIndex = 0;
+            this.cmdReinicio.Text = "&Reinicio de juego";
+            this.cmdReinicio.UseVisualStyleBackColor = true;
+            this.cmdReinicio.Click += new System.EventHandler(this.cmdReinicio_Click);
+            // 
+            // lblRecord
+            // 
+            this.lblRecord.AutoSize = true;
+            this.lblRecord.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecord.Location = new System.Drawing.Point(459, 55);
+            this.lblRecord.Name = "lblRecord";
+            this.lblRecord.Size = new System.Drawing.Size(72, 20);
+            this.lblRecord.TabIndex = 2;
+            this.lblRecord.Text = "Record:";
+            // 
+            // lblDijito
+            // 
+            this.lblDijito.AutoSize = true;
+            this.lblDijito.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDijito.Location = new System.Drawing.Point(537, 55);
+            this.lblDijito.Name = "lblDijito";
+            this.lblDijito.Size = new System.Drawing.Size(19, 20);
+            this.lblDijito.TabIndex = 3;
+            this.lblDijito.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 450);
+            this.ClientSize = new System.Drawing.Size(599, 332);
+            this.Controls.Add(this.lblDijito);
+            this.Controls.Add(this.lblRecord);
+            this.Controls.Add(this.cmdReinicio);
+            this.Controls.Add(this.paneljuego);
             this.Name = "Form1";
             this.Text = "Memorama";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel paneljuego;
+        private System.Windows.Forms.Button cmdReinicio;
+        private System.Windows.Forms.Label lblRecord;
+        private System.Windows.Forms.Label lblDijito;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- PROYECTO/Form1.cs
+++ PROYECTO/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,39 @@ namespace PROYECTO
 {
     public partial class Form1 : Form
     {
+        //parametros de como se  ver 
+        int TamanioColumnasFilas = 4;
+        int Movimientos = 0;
+        int CantidadDeCartasVolteadas = 0;
+        List<string> CartasEnumeradas;
+        List<string> CartasRevueltas;
+        ArrayList CartasSeleccionada;
+        PictureBox CartaTemporal1;
+        PictureBox CartaTemporal2;
+        int CartaActual = 0;
         public Form1()
         {
             InitializeComponent();
         }
+        public void Iniciarjuego()
+        {
+            timer1.Enabled = false;
+            timer1.Stop();
+            lblRecord.Text = "0";
+            CantidadDeCartasVolteadas = 0;
+            Movimientos = 0;
+            paneljuego.Controls.Clear();
+            CartasEnumeradas = new List<string>();
+            CartasRevueltas = new List<string>();
+            CartasSeleccionada = new ArrayList();
+
+        }
+
+        private void cmdReinicio_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
